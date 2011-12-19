ab21c36ebdf6f40dc3e90eb54cac11caeba24d92
Roché Compaan <roche@upfrontsystems.co.za>
2011-12-19 14:52:52 +0200

    use portal_transforms to transform xml to html

--- rhaptos/xmlfile/widget.py
+++ rhaptos/xmlfile/widget.py
@@ -1,11 +1,13 @@
 from xml.dom.minidom import parseString
 from zope.interface import implementsOnly, implementer
 from zope.component import adapts, adapter
+from zope.app.component.hooks import getSite
 
 from z3c.form.interfaces import IFormLayer, IFieldWidget
 from z3c.form.browser.widget import addFieldClass
 from z3c.form.widget import FieldWidget
 
+from plone.app.textfield.interfaces import ITransformer, TransformError
 from plone.app.textfield.widget import IRichTextWidget
 from plone.app.textfield.widget import RichTextWidget
 from plone.app.textfield.widget import RichTextConverter
@@ -26,10 +28,13 @@ class XMLTextWidget(RichTextWidget):
         super(RichTextWidget, self).update()
         addFieldClass(self)
 
-    def prettyprint(self):
-        dom = parseString(self.value.output)
-        return dom.toprettyxml()
-    
+    def html(self):
+        site = getSite()
+        transformer = ITransformer(site, None)
+        if transformer is None:
+            return None
+        return transformer(self.value, self.value.outputMimeType)
+
 
 @adapter(IXMLText, IFormLayer)
 @implementer(IFieldWidget)
