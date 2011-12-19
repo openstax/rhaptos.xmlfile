a047a286ae96d4ff327ca908416559c03d3485e6
Roché Compaan <roche@upfrontsystems.co.za>
2011-12-19 16:02:21 +0200

    always parse the raw value when validating the xml file

--- rhaptos/xmlfile/xmlfile.py
+++ rhaptos/xmlfile/xmlfile.py
@@ -3,10 +3,13 @@ from xml.parsers.expat import ExpatError
 
 from zope import schema
 from zope.interface import implements
+from zope.app.component.hooks import getSite
 
 from plone.directives import form
 from plone.app.textfield import RichText
 from plone.app.textfield.interfaces import IRichText, IRichTextValue
+
+from Products.CMFCore.utils import getToolByName
 
 from rhaptos.xmlfile import MessageFactory as _
 
@@ -40,10 +43,14 @@ class XMLText(RichText):
             )
 
     def _validate(self, value):
+        site = getSite()
+        properties = getToolByName(self, 'portal_properties', None)
+        site_properties = getattr(properties, 'site_properties', None)
+        charset = site_properties.getProperty('default_charset')
         try:
-            parseString(value.output)
+            parseString(value.raw.encode(charset))
         except ExpatError:
-            raise InvalidXML(value.output)
+            raise InvalidXML(value.raw)
         
 
 class IXMLFile(form.Schema):
