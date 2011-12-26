905c8d8f790787cda7e078bfa566c4e1df073d69
Roché Compaan <roche@upfrontsystems.co.za>
2011-12-26 11:59:23 +0200

    fix test name and doc string
    rename xmlfile to testfile to not confuse it with an instance of xmlfile

--- rhaptos/xmlfile/tests/test_xmlfile.py
+++ rhaptos/xmlfile/tests/test_xmlfile.py
@@ -5,7 +5,7 @@ from zope.component import createObject
 
 from Products.CMFCore.utils import getToolByName
 
-from rhaptos.xmlfile.xmlfile import IXMLFile, XMLText
+from rhaptos.xmlfile.xmlfile import IXMLFile, IXMLText, XMLText
 from rhaptos.xmlfile.value import XMLTextValue
 
 from base import PROJECTNAME
@@ -13,8 +13,8 @@ from base import INTEGRATION_TESTING
 
 dirname = os.path.dirname(__file__)
 
-class TestMarshal(unittest.TestCase):
-    """ Test marshal module """
+class TestXMLFile(unittest.TestCase):
+    """ Test xmlfile module """
     layer = INTEGRATION_TESTING
 
     def setUp(self):
@@ -25,7 +25,7 @@ class TestMarshal(unittest.TestCase):
         self.assertTrue(IXMLFile.providedBy(xmlfile))
 
     def test_xmltext(self):
-        xmlfile = open(os.path.join(dirname, 'test.cnxml')).read()
+        testfile = open(os.path.join(dirname, 'test.cnxml')).read()
         xmlfield = XMLText()
-        value = xmlfield.fromUnicode(xmlfile)
+        value = xmlfield.fromUnicode(testfile)
         self.assertTrue(isinstance(value, XMLTextValue))
